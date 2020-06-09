3a8d3350a1925976f628409fdaa2f31bb18c4218
iwatobipen <seritaka@gmail.com>
2020-06-09 20:30:56 +0900

    add razi

--- pychembldb/__init__.py
+++ pychembldb/__init__.py
@@ -349,6 +349,44 @@ class Version(Base):
     __table__ = Table('version', metadata, autoload=True)
 
 
+### structure/rdk schema
+
+try:
+    from razi import rdkit_postgresql
+    from razi.rdkit_postgresql.types import Mol
+    from razi.rdkit_postgresql.types import Bfp
+    from rdkit import Chem
+    metadata_rdk = MetaData(schema='rdk')
+    class Mols(Base):
+        __table__ = Table('mols',
+                          metadata_rdk,
+                          Column('molregno', BIGINT, primary_key=True),
+                          Column('m', Mol),
+                          extend_existing=True,
+                          )
+        __table_args__ = (
+            Index('molidx', 'structure',
+                        postgresql_using='gist'),
+            )
+    
+        def __repr__(self):
+            if isinstance(self.m, Chem.Mol):
+                return '(%s) < %s >' % (self.molregno, Chem.MolToSmiles(self.m))
+            return '(%s) < %s >' % (self.molregno, self.m)
+    
+    
+    class Fps(Base):
+        __table__ = Table('fps',
+                         metadata_rdk,
+                         Column('molregno', BIGINT, primary_key=True),
+                         Column('torsionbv', Bfp),
+                         Column('mfp2', Bfp),
+                         Column('ffp2', Bfp),
+                         extend_existing=True)
+except:
+    pass
+    
+
 ### Session
 chembldb = create_session(bind=engine)
 
